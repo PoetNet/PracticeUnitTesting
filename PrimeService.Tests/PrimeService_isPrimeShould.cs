9aef1ceec9dadfaf8848326da4cb45411624fb79
marfutin <130133417+marfutin@users.noreply.github.com>
2023-11-02 12:40:18 +0300

    Improve exampe, modify test, write README

--- PrimeService.Tests/PrimeService_isPrimeShould.cs
+++ PrimeService.Tests/PrimeService_isPrimeShould.cs
@@ -14,19 +14,45 @@ public class PrimeService_isPrimeShould
     }
 
     [TestMethod]
-    public void IsPrime_Inputs1_ReturnFalse()
+    public void IsPrime_InputsPrimes_ReturnTrue()
     {
-        bool result = _primeService.IsPrime(1);
-        Assert.IsFalse(result, "1 should not be prime");
+        int[] primeValues = { 5, 7, 11, 29, 43, 47 };
+
+        foreach (int value in primeValues)
+        {
+            bool result = _primeService.IsPrime(value);
+            Assert.IsTrue(result, $"{value} is prime");
+        }
+    }
+
+    [TestMethod]
+    [DataRow(2)]
+    [DataRow(3)]
+    public void IsPrime_ValuesLessThan3OrEqual3_ReturnTrue(int value)
+    {
+        bool result = _primeService.IsPrime(value);
+        Assert.IsTrue(result, $"{value} should not be prime");
     }
 
     [TestMethod]
     [DataRow(-1)]
     [DataRow(0)]
     [DataRow(1)]
-    public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+    public void IsPrime_InputsLessThan1OrEqual1_ReturnFalse(int value)
     {
         bool result = _primeService.IsPrime(value);
-        Assert.IsFalse(result, $"{value} should not be prime");
+        Assert.IsFalse(result, $"{value} is not prime");
+    }
+
+    [TestMethod]
+    public void IsPrime_InputsNotPrimes_ReturnTrue()
+    {
+        int[] primeValues = { 4, 8, 12, 33, 48, 74 };
+
+        foreach (int value in primeValues)
+        {
+            bool result = _primeService.IsPrime(value);
+            Assert.IsFalse(result, $"{value} is not prime");
+        }
     }
 }
--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -2,13 +2,18 @@
 
 public class PrimeService
 {
-    public bool IsPrime(int candidate)
-    {
-        if (candidate == 1)
-        {
-            return false;
-        }
+    public bool IsPrime(int number)
+    {        
+        if (number <= 1) return false;
+        if (number <= 3) return true;
+        if (number % 2 == 0 || number % 3 == 0) return false;
         
-        throw new NotImplementedException("Please create a test first");
+        for (int i = 5; i * i <= number; i += 6)
+        {
+            if (number % i == 0 || number % (i + 2) == 0)
+            { return false; }
+        }
+
+        return true;
     }
 }
